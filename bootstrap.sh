5df653a0f31b897fbba7f4efdc6076532056db63
earlofyor714 <alexphillipmartinez@gmail.com>
2017-08-14 19:17:12 -0700

    host on ngrok

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,18 +10,12 @@ if ! [ -L /var/www ]; then
 fi
 
 # python 3.6
-#sudo apt-get -qq install python3 python3-dev libtiff5-dev zlib1g-dev
-#curl -s https://bootstrap.pypa.io/get-pip.py | python3.4
-
-#sudo apt-get install -y software-properties-common python-software-properties
 sudo add-apt-repository -y ppa:jonathonf/python-3.6
 sudo apt-get update
 sudo apt-get install -y python3.6
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
 
-#sudo apt-get -qq install python3-dev libtiff5-dev zlib1g-dev
-#sudo apt-get install -y curl
 curl -s https://bootstrap.pypa.io/get-pip.py | python3.6
 sudo apt-get update
 
