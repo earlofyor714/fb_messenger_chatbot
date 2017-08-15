91d193aae0f8b8ee5bb59bb2cce97c1a45dc26ca
earlofyor714 <alexphillipmartinez@gmail.com>
2017-08-15 10:30:06 -0700

    matching tutorial code

--- application.py
+++ application.py
@@ -1,39 +1,29 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
-from json import dumps
-from flask_jsonpify import jsonify
-import requests
-
 import os
 import sys
 import json
 
+import requests
+from flask import Flask, request
+
 app = Flask(__name__)
-api = Api(app)
 
 
-class Hello_World(Resource):
-    def get(self):
-        if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-            if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
-                log("token mismatch")
-                return "Verification token mismatch", 403
-            log("hub challenge: {}".format(request.args["hub.challenge"]))
-            return request.args["hub.challenge"], 200
-        return "hello world", 200
+@app.route('/', methods=['GET'])
+def verify():
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        #if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+        if not request.args.get("hub.verify_token") == "boosted":
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
 
-
-class Food(Resource):
-    def get(self, value):
-        return "you input: {}".format(value)
+    return "Hello world", 200
 
 
 def log(message):
     print(str(message))
     sys.stdout.flush()
-
-api.add_resource(Hello_World, '/')
-api.add_resource(Food, '/food/<value>')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5002))
