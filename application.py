ca119153e6e947c834d9c14fbded342e0446bbfb
earlofyor714 <alexphillipmartinez@gmail.com>
2017-08-15 00:57:17 -0700

    uses  if available, else 5002

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Resource, Api
 from json import dumps
 from flask_jsonpify import jsonify
+import os
 
 app = Flask(__name__)
 api = Api(app)
@@ -20,4 +21,5 @@ api.add_resource(Hello_World, '/')
 api.add_resource(Food, '/food/<value>')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='33507')
+    port = int(os.environ.get('PORT', 5002))
+    app.run(host='0.0.0.0', port=port)
