1134edb33a96500709932b64149937092066fe3a
earlofyor714 <alexphillipmartinez@gmail.com>
2017-10-05 16:37:12 -0700

    updated google response

--- chatbot/Overhead.py
+++ chatbot/Overhead.py
@@ -13,11 +13,12 @@ class Overhead:
         response = "Google:\n"
         entities = self.googleInterpreter.analyzeEntities(message)
         for entity in entities:
-            response += "name: " + str(entity.name) + "\n"
-            log("entity:\n {}".format(str(entity)))
-            log("type:\n {0}".format(entity.type))
-            for md in entity.metadata:
-                log("metadata: {0}\n".format(md))
+            response += "name: " + str(entity.name) + "\n" + \
+                        "type: " + str(entity.type) + "\n" + \
+                        "wiki: " + str(entity.metadata['wikipedia_url']) + "\n" + \
+                        "salience: " + str(entity.salience)
+            log("entity: {}".format(str(entity)))
+            log("type: {0}".format(entity.type))
             log("md test: {0}".format(entity.metadata['wikipedia_url']))
             log("salience: {0}".format(entity.salience))
         return response
