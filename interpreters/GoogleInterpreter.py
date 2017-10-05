2a93f3f13d2fee6bcf7d699df69f8649f87b7e0b
earlofyor714 <alexphillipmartinez@gmail.com>
2017-10-05 14:17:34 -0700

    refactor Google code

--- interpreters/GoogleInterpreter.py
+++ interpreters/GoogleInterpreter.py
@@ -2,35 +2,35 @@ from google.cloud import language
 
 
 class GoogleInterpreter:
+    def __init__(self):
+        self.client = language.LanguageServiceClient()
+
     def interpret(self, message):
-        client = language.LanguageServiceClient()
         document = self.getDocument(message)
 
-        response = self.analyzeSentiment(client, document)
-        # response = self.analyzeEntities(client, document)
+        # response = self.analyzeSentiment(client, document)
+        response = self.analyzeEntities(document)
         # response = self.analyzeSyntax(client, document)
 
         return response
 
-    def analyzeSentiment(self, client, document):
-        response = client.analyze_sentiment(
+    def analyzeSentiment(self, document):
+        response = self.client.analyze_sentiment(
             document=document,
             encoding_type='UTF32'
         )
         sentiment = response.document_sentiment
-        # sendingMessage = "score: " + str(sentiment.score) + ", mag: " + str(sentiment.magnitude)
-        # return sendingMessage
         return str(sentiment)
 
-    def analyzeEntities(self, client, document):
-        response = client.analyze_entities(
+    def analyzeEntities(self, document):
+        response = self.client.analyze_entities(
             document=document,
             encoding_type='UTF32'
         )
         return str(response.entities)
 
-    def analyzeSyntax(self, client, document):
-        response = client.analyze_syntax(
+    def analyzeSyntax(self, document):
+        response = self.client.analyze_syntax(
             document=document,
             encoding_type='UTF32'
         )
