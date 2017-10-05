0647674567b56200a8c6a722c4e0dc05780ed078
earlofyor714 <alexphillipmartinez@gmail.com>
2017-10-05 14:56:54 -0700

    fix overhead

--- chatbot/Overhead.py
+++ chatbot/Overhead.py
@@ -13,8 +13,9 @@ class Overhead:
         response = "Google:\n"
         entities = self.googleInterpreter.analyzeEntities(message)
         for entity in entities:
-            response += "name: " + str(entity.name) + ",\n" + \
-                        "metadata: " + str(entity.metadata) + ",\n" + \
-                        "type: " + str(entity.metadata) + "\n"
+            #response += "name: " + str(entity.name) + ",\n" + \
+            #            "metadata: " + str(entity.metadata) + ",\n" + \
+            #            "type: " + str(entity.metadata) + "\n"
+            response += str(entity) + "\n"
 
         return response
