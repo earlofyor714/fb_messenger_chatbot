07fca123f84f61a4f28b6c3f503c0c08d71e2400
earlofyor714 <alexphillipmartinez@gmail.com>
2017-10-06 09:39:44 -0700

    made overhead global

--- application.py
+++ application.py
@@ -9,6 +9,8 @@ from chatbot.Overhead import Overhead
 
 app = Flask(__name__)
 
+chatbot = Overhead()
+
 
 @app.route('/', methods=['GET'])
 def verify():
@@ -40,7 +42,6 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
-                    chatbot = Overhead()
                     send_message(sender_id, chatbot.reply(message_text, log))
 
                 if messaging_event.get("delivery"):  # delivery confirmation
